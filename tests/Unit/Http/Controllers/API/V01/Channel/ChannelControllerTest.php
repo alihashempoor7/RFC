f1082872db676419195fd09cd0feeeb9ae095ce6
ali hashempoor <alihashempoor7@gmail.com>
2021-06-29 00:18:59 +0430

    create chnnel

---
+++ tests/Unit/Http/Controllers/API/V01/Channel/ChannelControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Http\Controllers\API\V01\Channel;
+
+use App\Http\Controllers\API\V01\Channel\ChannelController;
+use PHPUnit\Framework\TestCase;
+
+class ChannelControllerTest extends TestCase
+{
+
+    public function testIndex()
+    {
+
+    }
+}
