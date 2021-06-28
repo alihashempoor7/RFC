49da556adfbf61992070f62ee9c1acaa992a8a81
ali hashempoor <alihashempoor7@gmail.com>
2021-06-28 22:30:49 +0430

    second

--- app/Http/Controllers/API/V01/Auth/AuthController.php
+++ app/Http/Controllers/API/V01/Auth/AuthController.php
@@ -75,13 +75,6 @@ class AuthController extends Controller
     /**
      * @param Request $request
      */
-    public function create(Request $request): void
-    {
-        User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-    }
+
 
 }
